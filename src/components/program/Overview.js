85c8e0ad1a7aba7ff258c24ad79a45d7a7125dcf
Melf Clausen <melfclausen@MacBook-Pro.local>
2021-04-25 21:24:59 +0200

    nix

--- src/components/program/Overview.js
+++ src/components/program/Overview.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import workouts from '../../dummydata/browse-workouts';
 
 function Overview() {
   const location = useLocation();
+  console.log(location);
   const id = location.value;
   return <div>{workouts[id].title}</div>;
 }
